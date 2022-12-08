131c32775f15b3c3f99fb29792bbff57adbf4897
Dan Akmat <akmatdan@gmail.com>
2022-12-09 03:13:09 +0600

    Fixed bugs

--- FavFriend/ViewController.swift
+++ FavFriend/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
                              forCellReuseIdentifier: "Cell")
     }
 
-    @IBAction func save(_ sender: UIBarButtonItem) {
+    @IBAction func addName(_ sender: UIBarButtonItem) {
         
         let alert = UIAlertController(title: "New friend", message: "Add a new name", preferredStyle: .alert)
         
@@ -45,41 +45,58 @@ class ViewController: UIViewController {
     }
     
     func save(name: String) {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
-        
-        let person = NSManagedObject(entity: entity, insertInto: managedContext)
-        
-        person.setValue(name, forKeyPath: "name")
-        
-        do {
-            try managedContext.save()
-            people.append(person)
-        } catch let error as NSError {
-            print("Cpouldn't save. \(error), \(error.userInfo)")
-        }
+      
+      guard let appDelegate =
+        UIApplication.shared.delegate as? AppDelegate else {
+        return
+      }
+      
+      // 1
+      let managedContext =
+        appDelegate.persistentContainer.viewContext
+      
+      // 2
+      let entity =
+        NSEntityDescription.entity(forEntityName: "Person",
+                                   in: managedContext)!
+      
+      let person = NSManagedObject(entity: entity,
+                                   insertInto: managedContext)
+      
+      // 3
+      person.setValue(name, forKeyPath: "name")
+      
+      // 4
+      do {
+        try managedContext.save()
+        people.append(person)
+      } catch let error as NSError {
+        print("Could not save. \(error), \(error.userInfo)")
+      }
     }
+
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
+      super.viewWillAppear(animated)
+          
+        //1
+        guard let appDelegate =
+        UIApplication.shared.delegate as? AppDelegate else {
+          return
         }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-          
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
-          
+
+        let managedContext =
+        appDelegate.persistentContainer.viewContext
+
+        //2
+        let fetchRequest =
+        NSFetchRequest<NSManagedObject>(entityName: "Person")
+
+        //3
         do {
-            people = try managedContext.fetch(fetchRequest)
+        people = try managedContext.fetch(fetchRequest)
         } catch let error as NSError {
-            print("Couldn't fetch. \(error), \(error.userInfo)")
+        print("Could not fetch. \(error), \(error.userInfo)")
         }
     }
 }
